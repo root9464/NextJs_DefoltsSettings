a3fc70411a34ab61b734a705702346bde2295de5
iO <senpai9464@yandex.ru>
2024-05-03 16:21:14 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable new-cap */
+
+import {type Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.css';
-import { Metadata } from 'next';
 
 const inter = Inter({subsets: ['latin']});
 export const metadata: Metadata = {
@@ -11,9 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}: Readonly<{children: React.ReactNode}>) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>{children}</body>
